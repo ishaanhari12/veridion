9299f317b86c240db9b310b7da868146adddea3d
Ishaan Hari <ishaan.hari12@gmail.com>
2026-07-02 22:00:00 +0100

    fix: fixed bug in vpc file

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -70,7 +70,3 @@ resource "aws_route_table_association" "public_a" {
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table_association" "public_b" {
-  subnet_id      = aws_subnet.public_b.id
-  route_table_id = aws_route_table.public.id
-}
